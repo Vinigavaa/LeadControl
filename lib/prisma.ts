cbaaec279fb064918b7a2bd64bcd00a8cbbae606
vinicius.gava <vinigava14@gmail.com>
2026-02-10 16:45:07 -0300

    feat: Configurando banco, dependecias e ambiente

---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient({ adapter });
+} else {
+    const globalWithPrisma = global as typeof globalThis & {
+        prisma: PrismaClient;
+    }
+
+    if(!globalWithPrisma.prisma){
+        globalWithPrisma.prisma = new PrismaClient({ adapter });
+    }
+
+    prisma = globalWithPrisma.prisma;
+}
+
+export default prisma;
