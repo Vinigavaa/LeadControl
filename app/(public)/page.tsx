3a4f34ea629bc3842adb7d9dc5361634b3b9785c
Vinigavaa <vinigava14@gmail.com>
2026-02-11 23:10:03 -0300

    feat: alterações avulsas

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "./_components/hero";
 import BackgroundPattern from "@/components/background-pattern";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
